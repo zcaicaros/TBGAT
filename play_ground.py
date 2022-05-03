afb57c94c5a34d492fc2b16638cc16aaca1e6917
unknown <cong.zhang92@gmail.com>
2022-05-03 20:12:30 +0800

    add saved_model, log, and validation_data 20x20, results

--- play_ground.py
+++ play_ground.py
@@ -4,19 +4,10 @@ import torch
 from tqdm import tqdm
 
 
-# j = 30
-# m = 15
-# data = np.load('./validation_data/validation_data_and_Cmax_{}x{}_[1,99].npy'.format(j, m))
+j = 50
+m = 20
+data = np.load('./validation_data/validation_data_and_Cmax_{}x{}_[1,99].npy'.format(j, m))
+print(data[0, 1, 0])
 # data[:, 1, :, :] += 1
+# print(data[0, 1, 0])
 # np.save('./validation_data/validation_data_and_Cmax_{}x{}_[1,99].npy'.format(j, m), data)
-
-
-
-t = torch.tensor([True, False])
-print((~t).sum())
-
-torch.cat([torch.tensor(1), torch.tensor(1), torch.tensor(1), torch.tensor(1), None])
-
-print([None, None])
-if [None, None]:
-    print('yes')
