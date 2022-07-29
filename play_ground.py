9ca8e59d45f801742daec89f67e6bf0dc31c2275
#ZHANG CONG# <cong.zhang92@gmail.com>
2022-07-29 10:16:02 +0800

    upload extremly large-scale datasets

--- play_ground.py
+++ play_ground.py
@@ -11,3 +11,12 @@ print(data[0, 1, 0])
 # data[:, 1, :, :] += 1
 # print(data[0, 1, 0])
 # np.save('./validation_data/validation_data_and_Cmax_{}x{}_[1,99].npy'.format(j, m), data)
+
+j = 200
+m = 40
+data = np.load('./test_data/syn{}x{}.npy'.format(j, m))
+result = np.load('./test_data/syn{}x{}_result.npy'.format(j, m))
+time = np.load('./test_data/syn{}x{}_time.npy'.format(j, m))
+print(data[0].shape)
+print(result)
+print(time)
--- test_data/generate_random_instances.py
+++ test_data/generate_random_instances.py
@@ -2,10 +2,10 @@ import numpy as np
 from env.generateJSP import uni_instance_gen
 
 
-j = 150
-m = 25
+j = 1000
+m = 200
 l = 1
 h = 99
-size = 100
+size = 10
 instances = np.array([uni_instance_gen(n_j=j, n_m=m, low=l, high=h) for _ in range(size)])
 np.save('syn{}x{}.npy'.format(j, m), instances)
